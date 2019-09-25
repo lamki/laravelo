6d7f6293346cc913f777786f78dc83876505e71b
Zul Ikmal <zoulikmal92@gmail.com>
2019-09-26 00:59:35 +0800

    update

---
+++ jsonTest/test.php
@@ -0,0 +1,5 @@
+<?php
+	$arr = array("Name" => "Zul", "Location" => "Puchong", "Age" => "27", "Weight" => "70KG", "Height" => "5KM", "Hobby" =>array("Hby1" => "Makan", "Hby2" => "Tido", "Hby3" => "Lepak"));
+	echo json_encode($arr)."\n";
+
+?>
